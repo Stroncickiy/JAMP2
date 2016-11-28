aa1d493aa6f48176f12f549bfefcdd1f8db98195
Yaroslav_Strontsitskyy <Yaroslav_Strontsitskyy@epam.com>
2016-11-28 17:09:01 +0200

    added change to sockets app to print self messages

--- jamp-websockets-server/src/main/java/com/epam/websockets/client/Client.java
+++ jamp-websockets-server/src/main/java/com/epam/websockets/client/Client.java
@@ -1,17 +1,22 @@
 package com.epam.websockets.client;
 
 import java.net.URI;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 import javax.json.Json;
+import javax.websocket.DecodeException;
 import javax.websocket.Session;
 
 import org.glassfish.tyrus.client.ClientManager;
 
+import com.epam.websockets.decoder.MessageDecoder;
+import com.epam.websockets.model.Message;
+
 public class Client {
-
+	private static MessageDecoder decoder = new MessageDecoder();
 	public static final String SERVER = "ws://localhost:8025/ws/chat";
-
+    private static SimpleDateFormat simpleDateFormat = new SimpleDateFormat();
 	public static void main(String[] args) throws Exception {
 		ClientManager client = ClientManager.createClient();
 		String message;
@@ -26,8 +31,16 @@ public class Client {
 		// repeatedly read a message and send it to the server (until quit)
 		do {
 			message = scanner.nextLine();
-			session.getBasicRemote().sendText(formatMessage(message, user));
+			String jsonOfMessage = formatMessage(message, user);
+			session.getBasicRemote().sendText(jsonOfMessage);
+			printSelfMessage(jsonOfMessage);
 		} while (!message.equalsIgnoreCase("quit"));
+	}
+
+	private static void printSelfMessage(String jsonOfMessage) throws DecodeException {
+		Message messageObject = decoder.decode(jsonOfMessage);
+		  System.out.println(String.format("[%s:%s] %s",
+			         simpleDateFormat.format(messageObject.getReceived()), "ME", messageObject.getContent()));
 	}
 
 	private static String formatMessage(String message, String user) {
